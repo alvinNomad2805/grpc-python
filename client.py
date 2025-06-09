c69dfec5aa3e4f451c53305839b08d4f98891029
Alvin2805 <alvinsahroni@gmail.com>
2025-06-09 16:04:48 +0700

    check

--- client.py
+++ client.py
@@ -14,3 +14,22 @@ def run():
 
 if __name__ == '__main__':
     run()
+
+
+
+    import grpc
+import example_pb2,example_pb2_grpc
+
+def run():
+    channel = grpc.insecure_channel('localhost:50051')
+
+    stub = example_pb2_grpc.TestingStub(channel)
+
+    request = example_pb2.HelloRequest(name='checking message')
+
+    response = stub.sayHello(request)
+
+    print(f"the client received : {response.message}")
+
+if __name__ == '__main__':
+    run()
